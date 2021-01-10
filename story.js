ee4631fba7951d6bffa388d422a83d9eb3a4b0f9
SakeenaTraif <s.traif30@gmail.com>
2021-01-10 13:31:18 +0300

    Variables EX

---
+++ story.js
@@ -0,0 +1,6 @@
+const name="Sakeena";
+const age=100;
+const hobby="Coding";
+
+console.log(`Mr./Mrs. ${name}, in ${age} years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
+
